4f7d1729c20d1686ad72de1864545cbf3563b5dc
mlacher <mlacher@live.de>
2019-10-20 22:09:29 +0200

    second try

---
+++ app.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import datetime as dt
+import dash_daq as daq
+import weather as w
+
+from dash.dependencies import Input, Output
+
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(
+    __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
+)
+server = app.server
+colors = {
+    'background': '#0a0808',
+    'text': '#d1d2d6'
+}
+
+
+app.layout = html.Div(
+   # className = "3 columns",
+    children=[
+        html.Div(
+            id="graphs-container",
+            className="two rows",
+            children=[
+                html.Div(
+                    className="two columns",
+                    children=[
+                        html.Div(id='date'),
+                        html.Div(
+                            id="card-1",
+                            children=[
+                                daq.LEDDisplay(
+                                    id="operator-led",
+                                    value='12:34',
+                                    color="#92e0d3",
+                                    backgroundColor="#1e2130",
+                                    size=50
+                                ),
+                            ],
+                        ),
+                        
+                        dcc.Interval(
+                            id='interval-component',
+                            interval=1*1000, # in milliseconds
+                            n_intervals=0
+                        ),
+                        dcc.Graph(
+                        className="second column",
+                        figure={
+                            'data': [
+                                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                            ],
+                            'layout': {
+                                'plot_bgcolor': colors['background'],
+                                'paper_bgcolor': colors['background'],
+                                'font': {
+                                    'color': colors['text']
+                                    }
+                                }
+                        })
+                    ],
+                )
+            ],
+        )
+    ],
+)
+
+@app.callback(Output('operator-led', 'value'),
+              [Input('interval-component', 'n_intervals')])
+def update_LED(n):
+    time = dt.datetime.now().strftime('%H:%M')
+    return [
+        str(time)
+    ]
+
+
+@app.callback(Output('date', 'children'),
+              [Input('interval-component', 'n_intervals')])
+def update_time(n):
+    time = dt.datetime.now().strftime('%Y-%m-%d')
+    style = {'padding': '8px', 'fontSize': '24px'}
+    return [
+        html.Span(time, style=style)
+    ]
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ weather.py
@@ -0,0 +1,42 @@
+import pyowm
+import pandas as pd
+import numpy as np
+
+import plotly.express as px
+
+
+
+owm = pyowm.OWM('9f6afd479b6ab3d98a1ce2ed91815b0a')  # You MUST provide a valid API key
+test1 = []
+temp = []
+def fore_cast():
+    fc = owm.three_hours_forecast('Fürstenfeldbruck,GER')
+    f = fc.get_forecast()
+    for weather in f:
+        #print (weather.get_reference_time('iso'),weather.get_temperature('celsius'))
+        #test.append({'Time':weather.get_reference_time('iso'),'Temp':weather.get_temperature('celsius')})
+        date = weather.get_reference_time('iso')
+        new = weather.get_temperature('celsius')
+        temp.append([date, new['temp'], new['temp_max'],new['temp_min']])
+        
+    
+    return temp
+#observation = owm.weather_at_place('Fürstenfeldbruck,GER')
+#w = observation.get_weather()
+
+
+# Weather details
+#w.get_wind()                  # {'speed': 4.6, 'deg': 330}
+#w.get_humidity()              # 87
+#print(w.get_temperature('celsius'))  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+data1 = fore_cast()
+labels = ['date','temp','max','min']
+#data= np.array(data1).T.tolist()]
+
+data = pd.DataFrame(data1, columns = labels)
+#data = pd.DataFrame(data).set_index("n","b","q","s")
+print(data)
+
+fig = px.line(data ,  x='date', y=('temp'))
+
+fig.show()
