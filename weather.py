5c07e6354359ea8881d4b6bb3e0ebfd1301b1c91
mlacher <mlacher@live.de>
2019-10-23 14:13:03 +0200

    get current weather function added

--- weather.py
+++ weather.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import plotly.express as px
 
 
-
 owm = pyowm.OWM('9f6afd479b6ab3d98a1ce2ed91815b0a')  # You MUST provide a valid API key
 test1 = []
 temp = []
@@ -13,9 +12,7 @@ def fore_cast():
     fc = owm.three_hours_forecast('Fürstenfeldbruck,GER')
     f = fc.get_forecast()
     for weather in f:
-        #code is not nice need update soon
-        #print (weather.get_reference_time('iso'),weather.get_temperature('celsius'))
-        #test.append({'Time':weather.get_reference_time('iso'),'Temp':weather.get_temperature('celsius')})
+        #code is not nice requires update soon
         date_time = weather.get_reference_time()
         date = datetime.utcfromtimestamp(date_time).strftime('%Y-%m-%d')
         time = datetime.utcfromtimestamp(date_time).strftime('%H:%M')
@@ -24,26 +21,16 @@ def fore_cast():
         temp.append([date,time, alltemp['temp_max'],'temp_max'])
         temp.append([date,time, alltemp['temp_min'],'temp_min'])
         labels = ['date','time','temp','cat']
-        data_raw = pd.DataFrame(temp, columns = labels)
-        data = data_raw.groupby('date').max()
-        test = [data.index, data.time,data.temp]
-    return test
+        pd_data_raw = pd.DataFrame(temp, columns = labels)
+        pd_data_group = pd_data_raw.groupby('date').max()
+        data_forecast = [pd_data_group.index, pd_data_group.time,pd_data_group.temp]
+    return data_forecast
 #
-#
-
 def current_weather():
     observation = owm.weather_at_place('Fürstenfeldbruck,GER')
     w = observation.get_weather()
-    w.get_wind()                  # {'speed': 4.6, 'deg': 330}
-    w.get_humidity()              # 87
-    w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
-
-
-
-#files= fore_cast()
-
-
-#print(test[2][0:6].index)
-#fig = px.line(data1 ,  x='date', y=('temp'))
-
-#fig.show()
+    curr_wind=w.get_wind()                  # {'speed': 4.6, 'deg': 330}
+    #curr_hum= w.get_humidity()              # 87
+    curr_temp= w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+    curr_w = [curr_temp, curr_wind]
+    #print(curr_w[0]['temp']) example for current temperature
