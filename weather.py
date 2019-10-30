094672e92cdbfe7493134471f98cc9cc57140c2f
mlacher <mlacher@live.de>
2019-10-30 17:12:38 +0100

    update weather.py to enable better plots

--- weather.py
+++ weather.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import plotly.express as px
+import sys
+import plotly.express as px
 
 
 owm = pyowm.OWM('9f6afd479b6ab3d98a1ce2ed91815b0a')  # You MUST provide a valid API key
@@ -11,20 +13,23 @@ temp = []
 def fore_cast():
     fc = owm.three_hours_forecast('Fürstenfeldbruck,GER')
     f = fc.get_forecast()
+    labels = ['datetime','temp','cat']
     for weather in f:
         #code is not nice requires update soon
         date_time = weather.get_reference_time()
         date = datetime.utcfromtimestamp(date_time).strftime('%Y-%m-%d')
         time = datetime.utcfromtimestamp(date_time).strftime('%H:%M')
         alltemp = weather.get_temperature('celsius')
-        temp.append([date,time, alltemp['temp'],'temp'])
-        temp.append([date,time, alltemp['temp_max'],'temp_max'])
-        temp.append([date,time, alltemp['temp_min'],'temp_min'])
-        labels = ['date','time','temp','cat']
-        pd_data_raw = pd.DataFrame(temp, columns = labels)
-        pd_data_group = pd_data_raw.groupby('date').max()
-        data_forecast = [pd_data_group.index, pd_data_group.time,pd_data_group.temp]
-    return data_forecast
+        dateTime = date + ' '+ time
+        #print(dateTime)
+        temp.append([dateTime, alltemp['temp'],'temp'])
+        temp.append([dateTime, alltemp['temp_max'],'temp_max'])
+        temp.append([dateTime, alltemp['temp_min'],'temp_min'])
+        
+        #pd_data_group = pd_data_raw.groupby('date').max()
+        #data_forecast = [pd_data_group.index, pd_data_group.time,pd_data_group.temp]
+    pd_data_raw = pd.DataFrame(temp, columns = labels)
+    return pd_data_raw
 #
 
 def current_weather():
@@ -35,5 +40,9 @@ def current_weather():
     curr_temp= w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
     curr_w = [curr_temp, curr_wind, curr_hum]
     return  curr_w
-    #print(curr_w[2]) #example for humidity
-    #print(curr_w[0]['temp']) #example for temperature
+ 
+
+#to delete
+tips = fore_cast()
+fig = px.bar(tips[tips['cat']=='temp_max'], x="datetime", y="temp", color = "cat")
+fig.show()
