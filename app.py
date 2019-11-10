9bfafe1c21c65bce6bad7a5712dfc6e43ce17500
Maxi Lacher <mlacher@live.de>
2019-11-10 17:33:03 +0100

    style change

--- app.py
+++ app.py
@@ -95,8 +95,8 @@ card = dbc.Card(
 Time_LED = daq.LEDDisplay(
     id="operator-led",
     value='12:34',
-    #color="#92e0d3",
-    backgroundColor="#0a0808",
+    color="#E4085C",
+    backgroundColor="#000000",
     size=50
 )
 
@@ -105,8 +105,8 @@ News = dbc.Toast(
     [html.P(str(news[0]['title']), className="mb-0"),
     html.P(str(news[1]['title']), className="mb-0")],
     header="Spiegel News",
-    style={'width': '50%',
-            'color': '#7FDBFF',
+    style={'width': '100%',
+            'color': '#E4085C',
             'backgroundColor':'#000000',
             'font-size':'14px'}
 )
@@ -118,7 +118,9 @@ Temp_Graph = dcc.Graph(
             'x': temp_fc['datetime'],
             'y': temp_fc['temp'],
             'type': 'scatter',
-            'fill': 'tonexty'
+            'fill': 'tonexty',
+            'marker':{
+            'color': '#E4085C'}
         }],
         'layout': {
             'plot_bgcolor':'#000000',
@@ -183,7 +185,8 @@ app.layout = dbc.Container([
         dbc.Col(
             html.Div(id='date'),
             width={"size": 2, "offset": 5},
-            style={'text-align': 'center'}
+            style={'text-align': 'center',
+                'color':'#E4085C'}
             #style={'backgroundColor':'white'}
         ),
     ]),
@@ -191,11 +194,12 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(
             card,
-        width={'size':3,'offset':1},
+        width={'size':3},
         ),
         dbc.Col(
             News,        
-        width={'size':4, 'offset':4},
+        width={'size':2, 'offset':6},
+        #style={'backgroundColor':'white'}
         ),
     ]),
     dbc.Row(),
@@ -213,7 +217,7 @@ app.layout = dbc.Container([
 
         dbc.Col(
             maps,
-        width={'size':3, 'offset':3},
+        width={'size':3, 'offset':5},
         #style={'backgroundColor':'white'}
         )     
     ]),
